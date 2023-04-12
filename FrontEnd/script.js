1252f22b9478accabb23231de5176f395f730287
r78florea <riccardo.florea@gmail.com>
2023-04-12 15:53:54 +0200

    lol first
    Co-authored-by: philippekurz <philippekurz@users.noreply.github.com>

--- FrontEnd/script.js
+++ FrontEnd/script.js
@@ -1,66 +1,74 @@
-
-
 // Recuperation des travaux depuis le backend avec fetch
+const worksApi = await fetch(`http://localhost:5678/api/works`)
+const works = await worksApi.json()
+//console.log(works)
+
+// Recuperation des catégories depuis le backend avec fetch
+const categoriesApi = await fetch(`http://localhost:5678/api/categories`)
+const categories = await categoriesApi.json()
+//console.log(categories)
+
+afficheFiltres();
+afficheTravaux(0);
 
 
-// async function getWorks() {
-//     const response = await fetch(`http://localhost:5678/api/works`);
-//     const works = await response.json();
-//     console.log(works);
-// }  
-
-// getWorks();
-function getWorks() {
-    fetch(`http://localhost:5678/api/works`)
-    .then(resp => resp.json())
-    .then(data => afficheTravaux(data))
-    // .then(data => piecesFiltres(data))
-}
-
-
-function afficheTravaux(data) {
-
-    for (i of data) {
-
+// Fonction d'affichage des travaux
+function afficheTravaux(categorie) {
+    
     const sectionGallery = document.querySelector(".gallery");
-
-
-    const nomElement = document.createElement("figcaption");
-    const imageElement = document.createElement("img");
-    // imageElement.dataset.id = i.id;
-    // nomElement.dataset.id = i.id;
-
-
-    nomElement.innerHTML = i.title;
-    imageElement.src = i.imageUrl;
     
-    sectionGallery.appendChild(imageElement);
-    sectionGallery.appendChild(nomElement);
+    // Remise à zéro de la section gallery
+    sectionGallery.innerHTML = "";
     
-}
-}
-getWorks();
-afficheTravaux();
+    // Filtrage des travaux
+    let travauxFiltres = works;
+    if (categorie != 0) {
+        travauxFiltres = works.filter(function (work) {
+            return work.categoryId === categorie
+        })
+    }
 
-//     // const response = await fetch('http://localhost:5678/api/works')
-//         // const works = await response.json()
-
-
-// Creation des filtres 
-
-// const boutonTous = document.getElementById("btn-tous");
-// boutonTous.addEventListener("click", function () {
-//      getWorks();
-//      afficheTravaux();
-// })
-
-const boutonObjets = documents.querySelector("btn-objets");
-boutonObjets.addEventListener("click", function () {
-    
-    const objetsFiltres = data.filter(function (data) {
-        return data.categoryId = 1;
+    // Affichage des travaux
+    travauxFiltres.forEach(function (work) {    
+        const figure = document.createElement("figure")
+        const img = document.createElement("img");
+        const figCaption = document.createElement("figcaption");
+        img.src = work.imageUrl;
+        figCaption.innerHTML = work.title;
+        figure.appendChild(img);
+        figure.appendChild(figCaption);
+        sectionGallery.appendChild(figure);
     })
+      
+}
 
-    document.querySelector(".gallery").innerHTML = "";
-    afficheTravaux(objetsFiltres);
-})
+// Creation des filtres
+function afficheFiltres() {
+    
+    const sectionFiltres = document.querySelector(".filtres")
+    
+    // Création du bouton Tous
+    const button = document.createElement("button");
+    button.id = "btn-0";
+    button.innerText = "Tous";
+    button.classList.add("btn-filtres");
+    button.addEventListener("click", function () {
+        afficheTravaux(0)
+    });
+    sectionFiltres.appendChild(button)
+    
+    // Création des autres boutons
+    categories.forEach(function (categorie) {
+        const button = document.createElement("button");
+        button.id = "btn-" + categorie.id;
+        button.innerText = categorie.name;
+        button.classList.add("btn-filtres");
+        button.addEventListener("click", function () {
+            afficheTravaux(categorie.id)
+        });
+        sectionFiltres.appendChild(button);
+    }
+
+    )
+}
+
