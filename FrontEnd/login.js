9bfdfd1808f12c02d588d38c85532e9b9dbd1c27
r78florea <riccardo.florea@gmail.com>
2023-04-15 20:24:05 +0200

    connexion des logins

---
+++ FrontEnd/login.js
@@ -0,0 +1,44 @@
+//Creation de l'eventlistener sur le bouton se connecter
+const btnSubmit = document.querySelector("#submit");
+btnSubmit.addEventListener('click', (e)=> {
+    e.preventDefault();
+    connexion();
+})
+
+
+
+//Creation de la fonction de connexion
+
+async function connexion() {  
+
+    // stockage des donnes entrees par l'utilisateur
+    let utilisateur = document.getElementById("email").value;
+    let motDePasse = document.getElementById("mdp").value;
+    
+    // Creation de la fonction fetch post
+    let promise = await fetch('http://localhost:5678/api/users/login', {
+        method:"POST",
+        headers: {
+            accept: 'applicaton/json',
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            email: utilisateur,
+            password: motDePasse,
+        }),
+        })
+    let verif = await promise.json()
+    console.log(verif);
+    
+    // Gestion de la redirection en fonction de la verification
+    if (promise.status === 200) {
+        sessionStorage.setItem(1, verif.token);
+        document.location.href="./homepage_edit.html";
+    } else {
+        alert('Echec de la connexion: l\'e-mail ou le mot de passe sont incorrects')
+}
+
+}
+
+
+
